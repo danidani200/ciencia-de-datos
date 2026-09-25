e3d347b2790761b63d8c2ac4e0d87000730e4ed7
Danii <cuevasbaezg@gmail.com>
2026-09-25 18:22:25 +0000

    botondesexo

---
+++ botondesexo.py
@@ -0,0 +1,4 @@
+import streamlit as st
+
+if st.button("quiero sexo"):
+    st.write("has presionado el boton quiero sexo")
---
+++ data.py
@@ -0,0 +1,5 @@
+import streamlit as st
+import pandas as pd
+
+names_link = "https://github.com/danidani200/Autos/blob/main/%5Bluxa.org%5D-2.zip"
+names_data = pd.read_csv("")            
